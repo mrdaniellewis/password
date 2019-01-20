78c6e03f9cc95ec8e13ab0fd44937f64c461665a
Daniel Lewis <github@mrdaniellewis.co.uk>
2019-01-20 00:03:30 +0000

    fix caching urls

--- src/worker.js
+++ src/worker.js
@@ -1,31 +1,19 @@
 /* eslint-env serviceworker */
 /* eslint no-restricted-globals: "off" */
 
-const cacheName = 'cache-1';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    await cache.add('/');
-  })());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    await cache.add('https://cdn.polyfill.io/v2/polyfill.min.js');
-  })());
-});
+const cacheName = 'password-cache-1';
+const cacheDomains = [self.location.origin, 'https://cdn.polyfill.io'];
 
 self.addEventListener('fetch', (event) => {
+  if (!cacheDomains.includes(new URL(event.request.url).origin)) {
+    return;
+  }
   event.respondWith((async () => {
     const result = await caches.match(event.request.url);
-    if (result) {
-      Promise.resolve().then(async () => {
-        const cache = await caches.open(cacheName);
-        cache.add(event.request.url);
-      });
-    }
+    Promise.resolve().then(async () => {
+      const cache = await caches.open(cacheName);
+      cache.add(event.request.url);
+    });
     return result || fetch(event.request.url);
   })());
 });
